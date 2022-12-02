9fbd8f5e7b0a2009a45839cb7a5558895ca803ad
Lullaby330 <69coder@gmail.com>
2022-12-02 20:02:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5502075149:AAEvhd9C_MD9Paz3N2P7HpXLqRPmOeQOKhQ")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5324411690:AAGGPvK-PLb4XJpp2tIfN7Itf-Sr1VY09jU")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "6216349"))
@@ -19,20 +19,20 @@ APP_ID = int(os.environ.get("APP_ID", "6216349"))
 API_HASH = os.environ.get("API_HASH", "5c7418e9f3df6db931caa7354521c55f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001548998274"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001722604919"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5047866686"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1749162554"))
 
 #Port
-PORT = os.environ.get("PORT", "3040")
+PORT = os.environ.get("PORT", "3022")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://asubang:asubang@cluster0.gbyc7wo.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesasfbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "asdfvvd")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001891943867"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001676860169"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -40,7 +40,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nSaya Dapat Menyimpan File Pribadi Dan Pengguna Lain Dapat Mengaksesnya Dari Tautan Khusus.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5047866686 2072769117 2018095690 1850233338").split()):
+    for x in (os.environ.get("ADMINS", "1749162554").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
