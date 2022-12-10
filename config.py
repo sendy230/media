6a98b2cd6f633d53f66a1029d5d58306d89a5370
Lullaby330 <69coder@gmail.com>
2022-12-10 22:08:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfathera
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5850702556:AAEe157QEyQ514bR7g9S-OzyMT1iR0-i0bQ")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5619593995:AAFT3YPpCsd5eOzxjYv-TlGVXBoe_lrOtkc")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "6216349"))
@@ -19,20 +19,20 @@ APP_ID = int(os.environ.get("APP_ID", "6216349"))
 API_HASH = os.environ.get("API_HASH", "5c7418e9f3df6db931caa7354521c55f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100780905901"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001696659173"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1453626698"))
 
 #Port
-PORT = os.environ.get("PORT", "2020")
+PORT = os.environ.get("PORT", "0990")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://akusiapa:akusiapa@cluster0.3rh8jbk.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "wirasableng")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://asubang:asubang@cluster0.gbyc7wo.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "akupadanya")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001697856198"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001899683984"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -40,7 +40,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nSaya Dapat Menyimpan File Pribadi Dan Pengguna Lain Dapat Mengaksesnya Dari Tautan Khusus.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1453626698").split()):
+    for x in (os.environ.get("ADMINS", "5047866686 2072769117 2018095690 1850233338").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
