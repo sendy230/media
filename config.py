c0216d5d2c2909d40611a0cb9a97d3584e18229a
Lullaby330 <69coder@gmail.com>
2023-01-03 13:10:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5933627702:AAFT827viM-x9WkYiWQwogquCnCh2B-RPDA")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5519004782:AAExc6M8jzv2y0GVC5xaYziHkBTBvaGYw6s")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "6216349"))
@@ -19,20 +19,20 @@ APP_ID = int(os.environ.get("APP_ID", "6216349"))
 API_HASH = os.environ.get("API_HASH", "5c7418e9f3df6db931caa7354521c55f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001631836008"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001859283712"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "2018095690"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5047866686"))
 
 #Port
-PORT = os.environ.get("PORT", "1212")
+PORT = os.environ.get("PORT", "3256")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://asubang:asubang@cluster0.gbyc7wo.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "asdwwgggb")
+DB_NAME = os.environ.get("DATABASE_NAME", "iyokas")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001739301260"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001802939769"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -40,7 +40,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nSaya Dapat Menyimpan File Pribadi Dan Pengguna Lain Dapat Mengaksesnya Dari Tautan Khusus.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "2018095690").split()):
+    for x in (os.environ.get("ADMINS", "5776059273 5511211180 5530142462").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
