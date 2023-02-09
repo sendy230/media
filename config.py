c813cd2d4f8fbf58faee0e0acdf2d9cd422fa139
Lullaby330 <69coder@gmail.com>
2023-02-09 22:04:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ from logging.handlers import RotatingFileHandler
 
 
 
-#Bot token @Botfathera
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6117652075:AAEKlnrEL_6fNS7B9Y6FeUOVdRMhOJnCl-0")
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5431385717:AAGGaINbxWEo4FdSQvzpg7ZtxZ1mkJK2QdA")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "6216349"))
@@ -19,20 +19,20 @@ APP_ID = int(os.environ.get("APP_ID", "6216349"))
 API_HASH = os.environ.get("API_HASH", "5c7418e9f3df6db931caa7354521c55f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001673748751"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001781509079"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "1453626698"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5047866686"))
 
 #Port
-PORT = os.environ.get("PORT", "1819")
+PORT = os.environ.get("PORT", "3212")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://asubang:asubang@cluster0.gbyc7wo.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "s1")
+DB_NAME = os.environ.get("DATABASE_NAME", "asdsddds")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001769556559"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001844902804"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
